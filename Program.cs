184566e2004b994a2e9e597068c1dbba732bedec
chandrut <chandru9279@gmail.com>
2018-11-10 19:20:34 +0530

    perf spike

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+
+namespace spikes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseSerilog((context, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console())
+                .UseStartup<Startup>();
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace spikes
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseMiddleware<RequestLoggingMiddleware>();
+
+            app.UseMvc(routes =>
+                {
+                    routes.Routes.Clear();
+                    routes.Routes.Add(new Router());
+
+                });
+        }
+    }
+
+    public class Router : IRouter
+    {
+        public Task RouteAsync(RouteContext context)
+        {
+            return Task.Run(() =>
+            {
+                context.Handler = httpContext => context.HttpContext.Response.WriteAsync("SOME VALUE");
+            });
+        }
+
+        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+        {
+            return new VirtualPathData(this, "/myroute");
+        }
+    }
+}
